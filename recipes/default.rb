f298c585db045c7d91b00280f77e25165487cc77
Bryan Konowitz <bryan@kono.sh>
2015-07-28 18:51:35 -0700

    adding a check for mac osx to use homebrew, otherwise defaulting to original behavior

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,32 +1,38 @@
-include_recipe "git"
+case node["platform_family"]
+when 'mac_os_x'
+  include_recipe "homebrewalt::default"
+  package 'ruby-install'
+else
+  include_recipe "git"
 
-attributes = node["ruby-install"]
-git_url = attributes["git_url"]
-git_ref = attributes["git_ref"]
+  attributes = node["ruby-install"]
+  git_url = attributes["git_url"]
+  git_ref = attributes["git_ref"]
 
-install_path = attributes["install_path"]
-unless install_path
-  cache_path = Chef::Config["file_cache_path"]
-  install_path = "#{cache_path}/ruby-install"
-end
+  install_path = attributes["install_path"]
+  unless install_path
+    cache_path = Chef::Config["file_cache_path"]
+    install_path = "#{cache_path}/ruby-install"
+  end
 
-directory install_path do
-  recursive true
-  action :create
-end
+  directory install_path do
+    recursive true
+    action :create
+  end
 
-execute "Install ruby-install" do
-  cwd install_path
-  command %{sudo make install}
+  execute "Install ruby-install" do
+    cwd install_path
+    command %{sudo make install}
 
-  action :nothing
-end
+    action :nothing
+  end
 
-git install_path do
-  repository git_url
-  reference git_ref
+  git install_path do
+    repository git_url
+    reference git_ref
 
-  action :sync
+    action :sync
 
-  notifies :run, resources(execute: "Install ruby-install"), :immediately
+    notifies :run, resources(execute: "Install ruby-install"), :immediately
+  end
 end
